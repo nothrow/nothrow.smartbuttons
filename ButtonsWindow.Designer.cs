1a8917f9e4173d49c64b62b4e0b296d14282c68c
Vit Tauer <vit.tauer@solarwinds.com>
2017-10-20 12:34:56 +0200

    Initial commit

---
+++ ButtonsWindow.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace nothrow.smartbuttons
+{
+    partial class ButtonsWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ButtonsWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 332);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ButtonsWindow";
+            this.Opacity = 0.8D;
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ButtonsWindow_FormClosing);
+            this.Load += new System.EventHandler(this.ButtonsWindow_Load);
+            this.MouseEnter += new System.EventHandler(this.ButtonsWindow_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.ButtonsWindow_MouseLeave);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ ButtonsWindow.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nothrow.smartbuttons
+{
+    public partial class ButtonsWindow : Form
+    {
+        private const int RightPadding = 10;
+        private const int OpacitySpeed = 4;
+        private const int InvisibleOpacity = 10;
+        private const int VisibleOpacity = 80;
+        private int _targetOpacity;
+
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+
+        public ButtonsWindow()
+        {
+            InitializeComponent();
+
+            Microsoft.Win32.SystemEvents.DisplaySettingsChanged += SystemEventsOnDisplaySettingsChanged;
+
+            _targetOpacity = VisibleOpacity;
+            Opacity = VisibleOpacity / 100.0;
+            Task.Run(UpdateOpacityTask, _cts.Token);
+        }
+
+        private int OpacityPercentage
+        {
+            get => (int)(Opacity * 100);
+            set => Invoke((MethodInvoker)(() => Opacity = value / 100.0));
+        }
+
+        private async Task UpdateOpacityTask()
+        {
+            while (!_cts.IsCancellationRequested)
+            {
+                if (OpacityPercentage != _targetOpacity)
+                {
+                    OpacityPercentage += Math.Sign(_targetOpacity - OpacityPercentage) * OpacitySpeed;
+                    if (Math.Abs(_targetOpacity - OpacityPercentage) < OpacitySpeed)
+                        OpacityPercentage = _targetOpacity;
+                }
+
+                await Task.Delay(40);
+            }
+        }
+
+        private void SystemEventsOnDisplaySettingsChanged(object sender, EventArgs eventArgs)
+        {
+            MoveToProperLocation();
+        }
+
+        private void MoveToProperLocation()
+        {
+            Location = 
+                new Point(SystemInformation.VirtualScreen.Width - Width - RightPadding, SystemInformation.VirtualScreen.Height / 2 - Height / 2);
+        }
+
+        private void ButtonsWindow_Load(object sender, EventArgs e)
+        {
+            MoveToProperLocation();
+        }
+
+        private void ButtonsWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Microsoft.Win32.SystemEvents.DisplaySettingsChanged -= SystemEventsOnDisplaySettingsChanged;
+            _cts.Cancel();
+        }
+
+        private void ButtonsWindow_MouseEnter(object sender, EventArgs e)
+        {
+            _targetOpacity = InvisibleOpacity;
+        }
+
+        private void ButtonsWindow_MouseLeave(object sender, EventArgs e)
+        {
+            _targetOpacity = VisibleOpacity;
+        }
+    }
+}
